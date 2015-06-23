38786249017650815c9bedff1ea8592182d12242
Maxim Pervushin <maxim.pervushin@gmail.com>
2015-06-23 12:29:42 +0300

    Today extension minor change.

---
+++ HyperTimeLogger/Common/Src/Views/HTLRoundButton.h
@@ -0,0 +1,10 @@
+//
+// Created by Maxim Pervushin on 23/06/15.
+// Copyright (c) 2015 Maxim Pervushin. All rights reserved.
+//
+
+#import <UIkit/UIkit.h>
+
+
+@interface HTLRoundButton : UIButton
+@end
