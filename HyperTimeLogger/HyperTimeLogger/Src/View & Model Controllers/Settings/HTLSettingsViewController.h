dde2ddef29f8663d68302ed856327c68a40b7ab1
Maxim Pervushin <maxim.pervushin@gmail.com>
2016-02-12 14:38:30 +0300

    Fixed merge issues

---
+++ HyperTimeLogger/HyperTimeLogger/Src/View & Model Controllers/Settings/HTLSettingsViewController.h
@@ -0,0 +1,11 @@
+//
+// Created by Maxim Pervushin on 04/06/15.
+// Copyright (c) 2015 Maxim Pervushin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HTLSettingsViewController : UIViewController
+@end
+
